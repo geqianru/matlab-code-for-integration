b9e210fa9c910c84a06e38f006070d4543e6d37d
geqianru <geqianru@gmail.com>
2014-06-19 11:54:57 +0200

    Gamma distribution approximation for Penalty Cost
    Use the two moments of the downtime to fit the gamma distribution then
    approximate the penalty cost

---
+++ Penalty.m
@@ -0,0 +1,35 @@
+function [Pen]= Penalty(mu_1,mu_2,r_1,r_2)
+t = cputime; 
+T=10;
+D_0=(mu_1+mu_2)*10.*1.5;
+c_p=72000;
+s_1=mu_1.*T.*r_1;
+s_2=mu_2.*T.*r_2;
+display(sum(s_1+s_2));
+%s_3=Lambda_3.*T.*r_3;
+v_1=mu_1.*T.*r_1^2;
+v_2=mu_2.*T.*r_2^2;
+%v_3=Lambda_3.*T.*r_3^2;
+theta=sum(v_1+v_2)./sum(s_1+s_2);
+display(theta);
+k=(sum(s_1+s_2))^2./sum(v_1+v_2);
+display(k);
+% pd1 = makedist('Gamma','a',k,'b',theta);
+% m_1=mean(pd1);
+% display(m);
+% pd2 = makedist('Gamma','a',k+1,'b',theta);
+% m_2=mean(pd2); 
+% x = gamrnd(k,theta);
+% m_1=mean(x(x>D_0));
+% m_2=mean(x);
+% display(m_1);
+% display(m_2);
+P_1=gamcdf(D_0,k+1,theta);
+display(P_1);
+P_2=gamcdf(D_0,k,theta);
+display(P_2);
+Pen=c_p.*theta.*k.*(1-P_1)-D_0.*c_p.*(1-P_2);
+e = cputime-t;
+display(e); 
+display(Pen);
+end
