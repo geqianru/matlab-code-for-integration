29869a4fe4f0949d33f513015e501fce3076ec8b
geqianru <geqianru@gmail.com>
2014-07-03 11:15:18 +0200

    ri
    ri can varies between different components

--- approxiamtion of pc/PenGamU3.m
+++ approxiamtion of pc/PenGamU3.m
@@ -1,44 +1,58 @@
-function [Pen, e]= PenGamU3(n)
+function [Pen, e]= PenGamU3(D_0)
 %%%%%%%%%%%Use Gamma distribution to fit downtime distribution and lambda follow uniform distribution%%%%%%%%%%%%%%%%%
   t = cputime;
   T = 10;
+  n = 3;
   for i = 1:n
-       a_i(i) = 0.25;
-       b_i(i) = 0.75;
-       r_i(i) = 1;
-       mu_i(i) = 0.5 * (a_i(i) + b_i(i));
-       sem_i(i) = (T^2) * (r_i(i)^2) * (a_i(i)^2 + a_i(i) * b_i(i) + b_i(i)^2)/3;
-       s_i(i) = mu_i(i) * T * r_i(i);
-       v_i(i) = mu_i(i) * T * r_i(i)^2;
-    
+      if i<=1
+        as(i) = 0.25;
+        bs(i) = 0.75;
+        rs(i) = 2.5;
+      elseif (i<=2)&&(i>1)
+        as(i) = 0.15;
+        bs(i) = 0.65;
+        rs(i) = 2;
+      elseif (i<=3)&&(i>2)
+        as(i) = 0.10;
+        bs(i) = 0.60;
+        rs(i) = 1.5;
+      elseif (i>60)&&(i<=80)
+        as(i) = 0.15;
+        bs(i) = 0.45;
+        rs(i) = 1;
+      else (i>80)&&(i<=100)
+        as(i) = 0.10;
+        bs(i) = 0.40;
+        rs(i) = 1;
+      end 
+       mus(i) = 0.5 * (as(i) + bs(i));
+       sems(i) = (T^2) * (rs(i)^2) * (as(i)^2 + as(i) * bs(i) + bs(i)^2)/3;
+       ss(i) = mus(i) * T * rs(i);
+       vs(i) = mus(i) * T * rs(i)^2;  
   end
-     
-
-
-
-D_0 = sum (mu_i .* r_i).*T ;
+%D_0 = sum (mus.*rs).*T ;
 c_p = 72000;
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%first Moment%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-M_1 = sum (s_i);
-%%%%%%%%%%%%%%%%%%%%%%%Second Moment%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-M_21 = sum(sem_i);
-M_22 = sum(s_i)^2 - sum(s_i.^2);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%first Moment of downtime%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+M_1 = sum (ss);
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Second Moment of downtime%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+M_21 = sum(sems);
+M_22 = sum(ss)^2 - sum(ss.^2);
 %M_22 = 2 * s_1 * s_2;
-M_23 = sum(v_i);
+M_23 = sum(vs);
 M_2 = M_21 + M_22 + M_23;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Scale parameter%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
 theta = M_2 / M_1 - M_1;
-display (theta);
+%display (theta);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Shape parameter%%%%%%%%%%%%%%%%%%%%%%%%%%%
 k = M_1 / theta;
-display (k);
+%display (k);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%CDF of Gamma distribution%%%%%%%%%%%%%%%%%%%%%
-P_1=gamcdf(D_0,k+1,theta);
-display(P_1);
-P_2=gamcdf(D_0,k,theta);
-display(P_2);
-Pen=c_p.*theta.*k.*(1-P_1)-D_0.*c_p.*(1-P_2);
+P_1 = gamcdf(D_0,k+1,theta);
+%display(P_1);
+P_2 = gamcdf(D_0,k,theta);
+%display(P_2);
+Pen = c_p.*theta.*k.*(1-P_1)-D_0.*c_p.*(1-P_2);
 e = cputime-t;
 display(e); 
 display(Pen);
